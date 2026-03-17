31188d918b003ffab2a50f9e41fc17897881f80c
Ananya <ananyaananyaa124@gmail.com>
2026-03-17 19:52:05 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -1,8 +1,22 @@
-import cv2
 from ultralytics import YOLO
 
-def main():
-    model = YOLO('yolov8n.pt')
-    model.predict(source='sample.jpg', save=True)
+model = YOLO("yolov8n.pt")
 
-if __name__ == "__main__":
+model.train(
+    data="dataset/data.yaml",
+    epochs=50,
+    imgsz=640,
+    batch=8,
+    degrees=180,
+    translate=0.2,
+    scale=0.5,
+    shear=10,
+    perspective=0.001
+)
+results = model.predict(
+    source="/content/dataset/train/images/001-29-_jpeg.rf.07313a59b01c605e18ac4ced2e53961e.jpg",
+    conf=0.15,
+    show_labels=True,
+    show_conf=False,
+    save=True
+)
